8d49b4764008bf6b1c4ff300df7ff1351f016873
rajfruzsi <34722134+rajfruzsi@users.noreply.github.com>
2019-01-07 16:07:58 +0100

    Delete auth.py
    twitter tokens not in use anymore

--- auth.py
+++
@@ -1,4 +0,0 @@
-consumer_key = 'pZq6xYSByzpDF9YnbiOoB0bcp'  
-consumer_secret= 'Y4loC8w1yQPIf4mZ7X5zHNmMfc6DG6YLU1QTYusypTfhZ4KXZS'
-access_key= '1060151021962477569-9Bb5SiPWOnEJuOkMRbNfa08lCXVdMA'
-access_secret = 'gKWnozDlYIzKtHi6jxU2sMwOT41PU5iGuRd7ojBQN7p9X'
